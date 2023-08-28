152c5f29580efbd3b25a590ded9bc64eb42e377c
Anjela Hovakimyan <hovakimyan.0anjela@gmail.com>
2023-08-28 19:55:58 +0300

    homework 1

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -46,5 +46,28 @@ public class FigurePainter {
             }
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 5; k > i - 1; k--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 4; k > i; k--) {
+                System.out.print(" ");
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
