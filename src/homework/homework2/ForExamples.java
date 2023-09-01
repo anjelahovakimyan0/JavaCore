38f7300451505f6d1869074b859eba44dde93edc
Anjela Hovakimyan <hovakimyan.0anjela@gmail.com>
2023-09-01 21:12:07 +0300

    homework 2

--- src/homework/homework2/ForExamples.java
+++ src/homework/homework2/ForExamples.java
@@ -11,7 +11,6 @@ public class ForExamples {
         }
 
         System.out.println();
-        System.out.println();
 
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
@@ -19,7 +18,6 @@ public class ForExamples {
             }
         }
 
-        System.out.println();
         System.out.println();
 
         int[] array = {2, 5, 8, 4, 9, 3, 7};
