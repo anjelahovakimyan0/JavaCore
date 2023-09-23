ef08ebdceda562b4a58935bde719f50b6505d36a
Anjela Hovakimyan <hovakimyan.0anjela@gmail.com>
2023-09-23 15:45:29 +0300

    homework

--- src/homework/util/ArrayUtil.java
+++ src/homework/util/ArrayUtil.java
@@ -3,24 +3,29 @@ package homework.util;
 public class ArrayUtil {
 
     void print(int[] numbers) {
+        System.out.print("All numbers: ");
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
         }
     }
 
     void firstElement(int[] numbers) {
+        System.out.print("First element: ");
         System.out.println(numbers[0]);
     }
 
     void lastElement(int[] numbers) {
+        System.out.print("Last element: ");
         System.out.println(numbers[numbers.length - 1]);
     }
 
     void arrayLength(int[] numbers) {
+        System.out.print("Array length: ");
         System.out.println(numbers.length);
     }
 
     void minValue(int[] numbers) {
+        System.out.print("Min: ");
         int min = numbers[0];
 
         for (int i = 1; i < numbers.length; i++) {
@@ -32,6 +37,7 @@ public class ArrayUtil {
     }
 
     void middleValueOrValues(int[] numbers) {
+        System.out.print("Middle number: ");
         if (numbers.length <= 2) {
             System.out.println("Can't print middle value.");
         } else if (numbers.length % 2 == 0) {
@@ -43,6 +49,7 @@ public class ArrayUtil {
     }
 
     void countOfEvens(int[] numbers) {
+        System.out.print("Count of evens: ");
         int count = 0;
 
         for (int i = 0; i < numbers.length; i++) {
@@ -54,6 +61,7 @@ public class ArrayUtil {
     }
 
     void countOfOdds(int[] numbers) {
+        System.out.print("Count of odds: ");
         int count = 0;
 
         for (int i = 0; i < numbers.length; i++) {
@@ -65,6 +73,7 @@ public class ArrayUtil {
     }
 
     void sum(int[] numbers) {
+        System.out.print("Sum: ");
         double sum = 0;
 
         for (int i = 0; i < numbers.length; i++) {
@@ -74,12 +83,13 @@ public class ArrayUtil {
     }
 
     void average(int[] numbers) {
+        System.out.print("Average: ");
         int sum = 0;
 
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
-        System.out.println("Average = " + sum / numbers.length);
+        System.out.println(sum / numbers.length);
     }
 }
 
