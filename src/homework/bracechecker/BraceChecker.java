6b174611969755386a4ab4c98684ebf69e598e81
Anjela Hovakimyan <hovakimyan.0anjela@gmail.com>
2023-09-26 23:06:37 +0300

    classwork

--- src/homework/bracechecker/BraceChecker.java
+++ src/homework/bracechecker/BraceChecker.java
@@ -6,14 +6,14 @@ public class BraceChecker {
     private Stack stck = new Stack(10);
     int pop;
 
-    public  BraceChecker(String text){
+    public BraceChecker(String text) {
         this.text = text;
     }
 
-    public void check(){
+    public void check() {
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
-            switch(c){
+            switch (c) {
                 case '(':
                 case '{':
                 case '[':
@@ -21,32 +21,32 @@ public class BraceChecker {
                     break;
                 case ')':
                     pop = stck.pop();
-                    if(pop == 0){
+                    if (pop == 0) {
                         System.out.println("Error: Closed " + c + ", but not opened at " + i);
-                    }else if(pop != '('){
+                    } else if (pop != '(') {
                         System.out.println("Error: Opened " + (char) pop + ", but closed " + c + " at " + i);
                     }
                     break;
                 case '}':
                     pop = stck.pop();
-                    if(pop == 0){
+                    if (pop == 0) {
                         System.out.println("Error: Closed " + c + ", but not opened at " + i);
-                    }else if(pop != '{'){
+                    } else if (pop != '{') {
                         System.out.println("Error: Opened " + (char) pop + ", but closed " + c + " at " + i);
                     }
                     break;
                 case ']':
                     pop = stck.pop();
-                    if(pop == 0){
+                    if (pop == 0) {
                         System.out.println("Error: Closed " + c + ", but not opened at " + i);
-                    }else if(pop != '['){
+                    } else if (pop != '[') {
                         System.out.println("Error: Opened " + (char) pop + ", but closed " + c + " at " + i);
                     }
                     break;
 
             }
         }
-        while(stck.getSize() >= 0){
+        while (stck.getSize() >= 0) {
             System.out.println("Error: Opened " + (char) stck.pop() + " but not closed.");
         }
     }
