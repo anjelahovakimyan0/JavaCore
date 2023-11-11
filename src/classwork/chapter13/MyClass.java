713f69afccce8d6d3c0c1f4690a2c6f70b228984
Anjela Hovakimyan <hovakimyan.0anjela@gmail.com>
2023-11-11 22:54:07 +0300

    classwork

---
+++ src/classwork/chapter13/MyClass.java
@@ -0,0 +1,20 @@
+package classwork.chapter13;
+
+public class MyClass {
+
+    int a;
+    int b;
+
+    MyClass(int i, int j) {
+        a = i;
+        b = j;
+    }
+
+    MyClass(int i) {
+        this(i, i);
+    }
+
+    MyClass() {
+        this(0);
+    }
+}
