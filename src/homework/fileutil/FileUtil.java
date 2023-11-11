bd044e0835a2ebf8f7e86d3e19eb8dfc49a01fa6
Anjela Hovakimyan <hovakimyan.0anjela@gmail.com>
2023-11-11 22:53:49 +0300

    homework

--- src/homework/fileutil/FileUtil.java
+++ src/homework/fileutil/FileUtil.java
@@ -13,6 +13,7 @@ public class FileUtil {
         findLines();
         printSizeOfPackage();
         createFileWithContent();
+        ContentSearchWithRecursion.content();
     }
 
     static void fileSearch() {
